4d50c8952ef2a6dc2617a198e2bca0d4643c7f19
Stepan Agafonov <agafonov.stepan@gmail.com>
2023-05-02 23:58:22 +0700

    An app was made, having UIButton and UILabel

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var countLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Create the button
+        let button = UIButton(type: .system)
+        button.setTitle("Click Me", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        button.center = view.center
+        view.addSubview(button)
+
+        // Create the label to display the count
+        countLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        countLabel.center = CGPoint(x: view.center.x, y: view.center.y + 50)
+        countLabel.textAlignment = .center
+        countLabel.text = "Count: 0"
+        view.addSubview(countLabel)
     }
 
-
+    @objc func buttonTapped() {
+        count += 1
+        countLabel.text = "Count: \(count)"
+    }
 }
-
